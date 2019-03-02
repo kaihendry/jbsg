4749d9242de32c72888f4a206ddaf81c1e019582
Kai Hendry <hendry@webconverger.com>
2019-03-02 18:48:46 +0800

    Reading type Parsing time help from #go-nuts

--- main.go
+++ main.go
@@ -15,6 +15,11 @@ import (
 
 var views = template.Must(template.ParseGlob("templates/*.html"))
 
+type Reading struct {
+	Value     int // >100 is unhealthy by either measurement
+	Timestamp time.Time
+}
+
 func main() {
 	addr := ":" + os.Getenv("PORT")
 	app := mux.NewRouter()
@@ -24,7 +29,7 @@ func main() {
 	}
 }
 
-func johorReading() (pasirgudang int, err error) {
+func johorReading() (pasirgudang Reading, err error) {
 
 	// Air Pollutant Index of Malaysia
 	// http://apims.doe.gov.my/public_v2/api_table.html
@@ -36,25 +41,35 @@ func johorReading() (pasirgudang int, err error) {
 	if err != nil {
 		return pasirgudang, err
 	}
+
 	var aq MalaysiaAPI
 	err = json.NewDecoder(resp.Body).Decode(&aq)
 	if err != nil {
 		return pasirgudang, err
 	}
+
 	defer resp.Body.Close()
 	// log.Infof("%v", aq)
 
+	loc, _ := time.LoadLocation("Asia/Singapore")
+
+	// This feels daft, but whatever
 	for _, v := range aq.Two4HourAPI {
 		if v[1] == "Location" {
 			latest := v[len(v)-1]
-			log.Infof("Latest: %s", latest)
+			log.Infof("Timestamp: %s", latest)
+			latest = time.Now().Format("2006-01-02") + " " + latest
+			pasirgudang.Timestamp, err = time.ParseInLocation("2006-01-02 3:04PM", latest, loc)
+			if err != nil {
+				log.WithError(err).Error("failed to parse time")
+			}
 		}
 		if v[1] == "Pasir Gudang" {
 			latest := v[len(v)-1]
 			log.Infof("Latest: %s", latest)
-			_, err := fmt.Sscanf(latest, "%d**", &pasirgudang)
+			_, err = fmt.Sscanf(latest, "%d**", &pasirgudang.Value)
 			if err != nil {
-				return pasirgudang, err
+				log.WithError(err).Error("failed to scan value")
 			}
 			break
 		}
@@ -64,7 +79,7 @@ func johorReading() (pasirgudang int, err error) {
 
 }
 
-func singaporeReading() (north int, err error) {
+func singaporeReading() (northSingapore Reading, err error) {
 	type SingaporePM25 struct {
 		RegionMetadata []struct {
 			Name          string `json:"name"`
@@ -93,13 +108,16 @@ func singaporeReading() (north int, err error) {
 
 	resp, err := http.Get("https://api.data.gov.sg/v1/environment/pm25")
 	if err != nil {
-		return north, err
+		return northSingapore, err
 	}
 	var aq SingaporePM25
 	err = json.NewDecoder(resp.Body).Decode(&aq)
 	defer resp.Body.Close()
 	log.Infof("%v", aq)
-	return aq.Items[0].Readings.Pm25OneHourly.North, err
+
+	northSingapore.Value = aq.Items[0].Readings.Pm25OneHourly.North
+	northSingapore.Timestamp = aq.Items[0].UpdateTimestamp
+	return
 }
 
 func handleIndex(w http.ResponseWriter, r *http.Request) {
@@ -107,22 +125,20 @@ func handleIndex(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("X-Robots-Tag", "none")
 	}
 
-	sg, err := singaporeReading()
+	northSingapore, err := singaporeReading()
 	if err != nil {
-		log.WithError(err).Fatal("failed to get singapore reading")
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		log.WithError(err).Error("failed to get singapore reading")
 		return
 	}
 
 	johor, err := johorReading()
 	if err != nil {
-		log.WithError(err).Fatal("failed to get johor reading")
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		log.WithError(err).Error("failed to get johor reading")
 		return
 	}
 
-	err = views.ExecuteTemplate(w, "index.html", map[string]int{
-		"Singapore": sg,
+	err = views.ExecuteTemplate(w, "index.html", map[string]Reading{
+		"Singapore": northSingapore,
 		"Johor":     johor,
 	})
 
